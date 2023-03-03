2752f4f26bef66dc784c17c6101b158d8208ba0e
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-03-03 19:51:52 +0100

    dia 03/03/2023

--- src/main/kotlin/almacen/AguaMineral.kt
+++ src/main/kotlin/almacen/AguaMineral.kt
@@ -1,7 +1,7 @@
 package almacen
 
-class AguaMineral(id : Int, lit : Double, pre : Double, mar : String, manantialOrigen:String):Bebida(id, lit, pre, mar) {
-    var manantial = manantialOrigen
+class AguaMineral(id : Int, lit : Double, pre : Double, mar : String, manantOrigen:String):Bebida(id, lit, pre, mar) {
+    var manantialOrigen = manantOrigen
 
     override fun calcular(cantidad: Int): Double {
         return cantidad * precio
