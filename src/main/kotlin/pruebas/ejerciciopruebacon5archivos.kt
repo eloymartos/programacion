02874060c42f62380eb9360fb8c10b7eda00ac3f
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-03-24 18:06:57 +0100

    pruebicas

--- src/main/kotlin/pruebas/ejerciciopruebacon5archivos.kt
+++ src/main/kotlin/pruebas/ejerciciopruebacon5archivos.kt
@@ -2,6 +2,7 @@ package pruebas
 
 import java.io.File
 import java.util.*
+import kotlin.math.abs
 
 fun main(){
     val archivo1 = File("data/correo.odt")
@@ -12,5 +13,17 @@ fun main(){
     val lista = mutableListOf(archivo1, archivo2, archivo3, archivo4, archivo5)
     for (i in lista.sortedWith(compareByDescending { it.length() }) ) println("$i = ${i.length()}")
     println("")
-    for (i in lista.sortedWith(compareByDescending { it.lastModified() }) ) println("$i = ${Date(i.lastModified())}")
+
+    var lista2 = lista.sortedWith(compareByDescending { it.lastModified() })
+    var distancia1 = abs(lista2[0].lastModified() - lista2[1].lastModified())
+    var guardados = arrayOf(lista2[0], lista2[1])
+
+    for (i in 0..lista2.size-2){
+        if (abs(lista2[i].lastModified()-lista2[i+1].lastModified()) < distancia1) {
+            distancia1 = lista2[i].lastModified()-lista2[i+1].lastModified()
+            guardados[0] = lista2[i]
+            guardados[1] = lista2[i+1]
+        }
+    }
+    println("$distancia1, que es la distancia entre ${guardados[0]} y ${guardados[1]}")
 }
