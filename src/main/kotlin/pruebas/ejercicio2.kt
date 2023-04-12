928967fc274aef8d00e860c2a3ccc544a008ebc9
eloymartos <115500079+eloymartos@users.noreply.github.com>
2023-04-12 21:26:25 +0200

    dos formas para el 2 de escritura

--- src/main/kotlin/pruebas/ejercicio2.kt
+++ src/main/kotlin/pruebas/ejercicio2.kt
@@ -3,12 +3,16 @@ package pruebas
 import java.io.*
 
 fun main(){
-    var cadenat =""
+    var cadena =""
     val lista1 = File("data/texto.txt").bufferedReader().readLines()
     val lista2 = File("data/ejemplo.txt").bufferedReader().readLines()
-    for (i in lista1) cadenat +=("$i\n")
-    for (i in lista2) cadenat +=("$i\n")
+    //File("data/ejemplo2.txt").printWriter().use{out->
+    //    for (i in lista1) out.println(i)
+    //    for (i in lista2) out.println(i)
+    //}
+    for (i in lista1) cadena +=("$i\n")
+    for (i in lista2) cadena +=("$i\n")
     val escritor = PrintWriter(File("data/ejemplo2.txt"))
-    escritor.append(cadenat)
+    escritor.append(cadena)
     escritor.close()
 }
